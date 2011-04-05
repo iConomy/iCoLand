701204424dcf62d52199f42e6ff937cd4ea8996f
magik <magiik@gmail.com>
2011-04-04 22:47:30 -0700

    cleanup

--- start.bat
+++ start.bat
@@ -1,3 +1,2 @@
-java -server -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y -cp craftbukkit.jar org.bukkit.craftbukkit.Main
-
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y -cp craftbukkit.jar org.bukkit.craftbukkit.Main
 pause
